d3599edd916f88617b44dc07a6087af682a89219
Chijioke1996 <111643913+Chijioke1996@users.noreply.github.com>
2023-05-16 15:12:52 +0100

    final commit

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express")
 const app = express()
 const port = 3000
 const mongoose = require("mongoose")
+const Document = require("./model/Document")
 
 app.set("view engine", "ejs")
 app.use(express.static("./public"))
+app.use(express.urlencoded({ extended: true}))
 
 const db = mongoose.connection
 mongoose.connect("mongodb://localhost/hastebin")
@@ -18,22 +20,54 @@ db.once("open", () => {
 
 app.get("/", (req, res) => {
     const code = 
-    `  Hey dear,
-    We've missed you dearly
-    come back home as soon as you can.
-    come back home as soon as you can.
-    come back home as soon as you can.
-    come back home as soon as you can.
-    come back home as soon as you can.
-    come back home as soon as you can.`
+    `  Welcome to Hastein!,
+    Use the commands in the top right corner 
+    to create a new file to share with others.`
 
-    res.render("code_display", {code})
+    res.render("code_display", {code, language : "plaintext"})
 })
 
 app.get("/new", (req, res) => {
 
     res.render("new")
 })
+
+app.post("/save", async (req, res) => {
+    const value = req.body.value
+    try {
+       const document =  await Document.create({value})
+       console.log(document );
+       res.redirect(`/${document.id}`)
+    } catch (error) {
+        res.render("new", { value})
+    }
+})
+
+app.get("/:id", async (req, res) => {
+    const id = req.params.id
+
+    try {
+       const document = await Document.findById(id) 
+       res.render("code_display", { code : document.value})
+    } catch (error) {
+        res.redirect("/")
+    }
+}) 
+
+// app.get("/:id/duplicate", async (req, res) => {
+  
+//     const id = req.params.id
+
+//     try {
+//        const document = await Document.findById(id) 
+//        res.render("new", { value : document.value})
+//     } catch (error) {
+//         res.redirect(`${id}`)
+//     }
+
+// })  
+
+
 
 
 
